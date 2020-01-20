cac1377216d6c9ef28cc33d3b8ccf6cc259b2caa
Nehal J Wani <nehaljw.kkd1@gmail.com>
2020-01-20 11:14:54 -0500

    Update bld.bat

--- recipes/mysql-connector-cpp/bld.bat
+++ recipes/mysql-connector-cpp/bld.bat
@@ -1,3 +1,5 @@
+GOTO THEREALDEAL
+
 dir "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140\"
 dir "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\"
 dir "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\"
@@ -24,6 +26,36 @@ set
 
 set "VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140"
 
+:THEREALDEAL
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET "MSBUILD_LOCATION=MSBuild.exe"
+
+for /D %%A IN ("%PROGRAMFILES(X86)%\Microsoft Visual Studio\*") DO (
+  for /D %%B IN ("%%~A\*") DO (
+    for /D %%C IN ("%%~B\MSBuild\*") DO (
+      SET "_POTENTIAL_MSBUILD_CANDIDATE=%%C\bin\msbuild.exe"
+      if EXIST "!_POTENTIAL_MSBUILD_CANDIDATE!" (
+ SET "MSBUILD_LOCATION=!_POTENTIAL_MSBUILD_CANDIDATE!"
+        GOTO STOP_MSBUILD_SEARCH
+      )
+    )
+  )
+)
+
+for /D %%A IN ("%PROGRAMFILES(X86)%\MSbuild\*") DO (
+  SET "_POTENTIAL_MSBUILD_CANDIDATE=%%A\bin\msbuild.exe"
+  if EXIST "!_POTENTIAL_MSBUILD_CANDIDATE!" (
+    SET "MSBUILD_LOCATION=!_POTENTIAL_MSBUILD_CANDIDATE!"
+    GOTO STOP_MSBUILD_SEARCH
+  )
+)
+
+:STOP_MSBUILD_SEARCH
+
+echo 'wtf is going on?'
+echo !MSBUILD_LOCATION!
+
 for %%O in (ON OFF) DO ( 
    cmake -S. ^
      -Bbuild.%%O ^
@@ -33,7 +65,8 @@ for %%O in (ON OFF) DO (
      -DWITH_SSL=%LIBRARY_PREFIX% ^
      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+     -DCMAKE_MAKE_PROGRAM=!MSBUILD_LOCATION!
      REM -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
    if ERRORLEVEL 1 EXIT 1
 
