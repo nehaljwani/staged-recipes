5c2c640ece8389223d6ef4accd506a75cd939ee0
Nehal J Wani <nehaljw.kkd1@gmail.com>
2020-05-18 04:16:12 -0400

    Revert gold and add another dep

--- recipes/wxwidgets/build.sh
+++ recipes/wxwidgets/build.sh
@@ -1,11 +1,7 @@
-# Use Gold linker and -Wl,--as-needed as suggested in
-# https://github.com/conda-forge/staged-recipes/pull/11589#issuecomment-629882668
-export LDFLAGS="${LDFLAGS:-} -fuse-ld=gold -Wl,--as-needed"
-
 ./configure \
   --prefix=${PREFIX} \
   --with-gtk="3" \
-  --with-opengl
+  --with-opengl || (cat config.log && exit -1)
 
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 
